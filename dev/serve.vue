31702e29c53d08337f813abb26165dc48d94d440
Parker Lee <plee@parkers-mbp.attlocal.net>
2022-07-09 22:40:15 -0500

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { ToolkitSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  ToolkitSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <toolkit-sample />
+  </div>
+</template>
